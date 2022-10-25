c046644342b642c83f68e70d069758058a0c7f6b
Rhet Turnbull <rturnbull@gmail.com>
2022-10-25 06:53:12 -0700

    Bumped version

--- src/textinator.py
+++ src/textinator.py
@@ -40,7 +40,7 @@ from pasteboard import TIFF, Pasteboard
 from utils import get_app_path, verify_desktop_access
 
 # do not manually change the version; use bump2version per the README
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 APP_NAME = "Textinator"
 APP_ICON = "icon.png"
